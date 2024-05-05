7d67a0f33550fcb85c8eaaeff9324d92948ce4e5
Nifemi-git <nifemiakingba@gmail.com>
2024-05-05 08:40:08 +0100

    improve functionality

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const inputElem = document.getElementById("display");
-const BUTTONS = document.getElementsByClassName("output-btn");
+const buttonsArr = Array.from(document.getElementsByClassName("output-btn"));
+const operatorsArr = Array.from(document.getElementsByClassName("op-btn"));
 const equalsBtn = document.getElementById("equals-btn");
 
 const handleButton = (button) => {
-  if (inputElem.value === "0" && button.value !== ".") {
+  const result = operatorsArr.every((element) => button.value !== element.value);
+  if (inputElem.value === "0" && result) {
     inputElem.value = button.value;
   } else {
     inputElem.value += button.value;
   }
 };
-Array.from(BUTTONS).forEach((btn) => {
+buttonsArr.forEach((btn) => {
   btn.onclick = () => handleButton(btn);
 });
 
